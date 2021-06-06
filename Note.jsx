490f4a1e8274499de2393a93c7887b7a0849cb16
bhargav4657 <56783402+bhargav4657@users.noreply.github.com>
2021-06-06 10:44:33 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Bootcamp</h1>
+      <p>
+        Learn to code with our beginner-friendly tutorials and examples. Read
+        tutorials, try examples, write programs, and learn to code.
+      </p>
+    </div>
+  );
+}
+export default Note;
